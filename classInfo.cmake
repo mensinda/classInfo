3f4ad2f19c74380302843943dcc160c1f80ede15
mensinda <daniel@mensinger-ka.de>
2016-06-19 12:38:25 +0200

    Added ooCMake dependency

--- classInfo.cmake
+++ classInfo.cmake
@@ -1,7 +1,12 @@
 cmake_minimum_required(VERSION 3.5)
 
 if( NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/cmake/" )
-  message( FATAL_ERROR "Unable to locate classInfo source files (${CMAKE_CURRENT_LIST_DIR}/cmake/ does not exist" )
+  message( FATAL_ERROR "Unable to locate classInfo source files (${CMAKE_CURRENT_LIST_DIR}/cmake/ does not exist)" )
 endif()
 
-message( STATUS ${CMAKE_CURRENT_LIST_DIR} )
+if( NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/ooCMake/ooCMake.cmake" )
+  message( FATAL_ERROR "Unable to locate ooCMake source files (${CMAKE_CURRENT_LIST_DIR}/ooCMake/ooCMake.cmake)" )
+endif()
+
+include( "${CMAKE_CURRENT_LIST_DIR}/ooCMake/ooCMake.cmake" )
+ooCM_init()
